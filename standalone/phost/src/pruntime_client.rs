8104372e4d1fad4fe1b61c935ccf08e82721ed50
Kevin Wang <wy721@qq.com>
2021-06-30 23:24:55 +0800

    phost: switch to bin_api to dispatch blocks

--- standalone/phost/src/pruntime_client.rs
+++ standalone/phost/src/pruntime_client.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
+use codec::Encode;
 use serde::Serialize;
 use hyper::Client as HttpClient;
 use hyper::{Body, Method, Request};
 use log::info;
 use bytes::buf::BufExt as _;
+use sp_runtime::DeserializeOwned;
 
 use crate::types::{
     RuntimeReq, Resp, SignedResp, ReqData, QueryReq, QueryRespData, Query, Payload
@@ -49,6 +51,36 @@ impl PRuntimeClient {
         let payload = RuntimeReq::new(request);
         let resp = self.req(command, &payload).await?;
         let result: Req::Resp = serde_json::from_str(&resp.payload)?;
+        Ok(result)
+    }
+
+    async fn bin_req<T: Encode>(&self, command: &str, param: &T) -> Result<SignedResp> {
+        let client = HttpClient::new();
+        let endpoint = format!("{}/{}", self.base_url, command);
+        let body = param.encode();
+
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri(endpoint)
+            .body(Body::from(body))?;
+
+        let res = client.request(req).await?;
+
+        info!("Response: {}", res.status());
+
+        let body = hyper::body::aggregate(res.into_body()).await?;
+        let signed_resp: SignedResp = serde_json::from_reader(body.reader())?;
+
+        Ok(signed_resp)
+    }
+
+    pub async fn bin_req_decode<Req: Encode, Resp: DeserializeOwned>(
+        &self,
+        command: &str,
+        request: Req,
+    ) -> Result<Resp> {
+        let resp = self.bin_req(command, &request).await?;
+        let result: Resp = serde_json::from_str(&resp.payload)?;
         Ok(result)
     }
 
